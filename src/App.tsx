02f5767e19003630d2ff52e9946f732263e54d60
hoonVelop-shin <78241389+hoonVelop-shin@users.noreply.github.com>
2021-08-06 18:12:57 +0900

    Login 뷰

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./components/Login";
 
 function App() {
-  return <div className="App">링크드인 클론 시작</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
